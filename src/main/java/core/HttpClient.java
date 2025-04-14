a6c2385d59fc3a1b75b019e894acec26dd610d30
Aninoss <github@aninoss.dev>
2025-04-14 15:56:18 +0200

    Timeout increased to 30s

--- src/main/java/core/HttpClient.java
+++ src/main/java/core/HttpClient.java
@@ -1,12 +1,12 @@
 package core;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InterruptedIOException;
+import java.net.InetAddress;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +15,27 @@ public class HttpClient {
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpClient.class);
     public static final String USER_AGENT = "Lawliet Discord Bot by aninoss";
 
-    private final OkHttpClient client = new OkHttpClient.Builder()
-            .callTimeout(10, TimeUnit.SECONDS)
-            .build();
+    private final OkHttpClient client;
+
+    public HttpClient() {
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(999);
+        dispatcher.setMaxRequestsPerHost(999);
+        ConnectionPool connectionPool = new ConnectionPool(100, 5, TimeUnit.MINUTES);
+        Dns dns = hostname -> Arrays.asList(InetAddress.getAllByName(hostname));
+
+        this.client = new OkHttpClient.Builder()
+                .dns(dns)
+                .connectionPool(connectionPool)
+                .dispatcher(dispatcher)
+                .callTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .cache(null)
+                .protocols(List.of(Protocol.HTTP_1_1))
+                .build();
+    }
 
     public HttpResponse request(String url, List<String[]> headers) {
         String domain = url.split("/")[2];
